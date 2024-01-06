c89517ad455b072b5f0c6b5a5f7bf1e20ba52a0b
Aniket Kumar Ghosh <ghoshaniketkumar7@gmail.com>
2024-01-06 21:57:28 +0530

    created a completely different resource as credential

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,10 @@ import { AppService } from "./app.service";
 import { SheetModule } from "./sheet/sheet.module";
 import TradingModule from "./trading/trading.module";
 import { ScheduleModule } from "@nestjs/schedule";
+import { DataSource } from "typeorm";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Credential } from "./credential/entities/credential.entity";
+import { CredentialModule } from "./credential/credential.module";
 
 @Module({
     imports: [
@@ -15,8 +19,23 @@ import { ScheduleModule } from "@nestjs/schedule";
         SheetModule,
         TradingModule,
         ScheduleModule.forRoot(),
+        TypeOrmModule.forRootAsync({
+            useFactory: () => ({
+                type: "mysql",
+                host: process.env.DB_HOST,
+                port: parseInt(process.env.DB_PORT) || 3306,
+                username: process.env.DB_USER,
+                password: process.env.DB_PASSWORD,
+                database: process.env.DB_NAME,
+                entities: [Credential],
+                synchronize: false,
+            }),
+        }),
+        CredentialModule,
     ],
     controllers: [AppController],
     providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+    constructor(private readonly dataSource: DataSource) {}
+}
