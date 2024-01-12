6ff2a24485517e86fc2f016c87eb471aaa80d1d0
Aniket Kumar Ghosh <ghoshaniketkumar7@gmail.com>
2024-01-12 23:36:42 +0530

    added created and updated column for credential entity

--- src/user/entities/credential.entity.ts
+++ src/user/entities/credential.entity.ts
@@ -1,5 +1,5 @@
 import AbstractEntity from "src/database/abstract.entity";
-import { Column, Entity, JoinColumn, ManyToOne } from "typeorm";
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, UpdateDateColumn } from "typeorm";
 import { DematAccount } from "./demat-account";
 
 //docs: https://dev.to/marienoir/understanding-relationships-in-typeorm-4873
@@ -12,7 +12,7 @@ export class Credential extends AbstractEntity<Credential> {
 
     @Column({
         name: "key_value",
-        length: 4000 // max size of a jwt token can be 7kb
+        length: 4000, // max size of a jwt token can be 7kb
     })
     keyValue: string;
 
@@ -23,4 +23,15 @@ export class Credential extends AbstractEntity<Credential> {
         name: "account_id",
     })
     account: DematAccount;
+
+    // Add these columns for automatic timestamp tracking
+    @CreateDateColumn({ type: "timestamp", default: () => "CURRENT_TIMESTAMP(6)" })
+    createdAt: Date;
+
+    @UpdateDateColumn({
+        type: "timestamp",
+        default: () => "CURRENT_TIMESTAMP(6)",
+        onUpdate: "CURRENT_TIMESTAMP(6)",
+    })
+    updatedAt: Date;
 }
