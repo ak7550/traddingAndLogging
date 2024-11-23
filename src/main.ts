30e7d7ce8582c21fecbc1a75252cda6c727e20c4
Aniket <aghosh@netapp.com>
2024-11-23 13:37:23 +0530

    adding APP_PORT in env file.

--- src/main.ts
+++ src/main.ts
@@ -32,7 +32,7 @@ async function bootstrap() {
 
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup( "api", app, document );
-    const port = process.env.PORT || 3000;
+    const port = process.env.APP_PORT || 3000;
     app.useLogger( new CustomLogger('NestApplication') );
     await app.listen(port, () => console.log(`app is running on port ${port}`));
 }
