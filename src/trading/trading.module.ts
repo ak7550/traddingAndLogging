68e48c879cfad6798ebef86ae52988457e5c7627
Aniket <aghosh@netapp.com>
2024-12-22 10:32:10 +0530

    unnecessary imports removed from trading module.

--- src/trading/trading.module.ts
+++ src/trading/trading.module.ts
@@ -2,14 +2,12 @@ import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { CustomLogger } from "../custom-logger.service";
 import { EntityModule } from "../entities/entity.module";
-import { UserModule } from "../entities/user/user.module";
 import { StockDataModule } from "../stock-data/stock-data.module";
 import AngelModule from "./angel/angel.module";
 import DhaanModule from "./dhaan/dhaan.module";
 import TradingFactoryService from "./trading-factory.service";
 import TradingController from "./trading.controller";
 import { TradingService } from "./trading.service";
-import { DematModule } from "src/entities/demat/demat.module";
 
 @Module({
     providers: [TradingFactoryService, TradingService, CustomLogger],
@@ -21,9 +19,7 @@ import { DematModule } from "src/entities/demat/demat.module";
         DhaanModule,
         AngelModule,
         EntityModule,
-        UserModule,
-        StockDataModule,
-        DematModule
+        StockDataModule
     ],
     exports: [CustomLogger]
 })
